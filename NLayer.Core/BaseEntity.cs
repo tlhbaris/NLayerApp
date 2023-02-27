49b73d3b1385119df04c29dce0d57598364dbe89
Talha Barış <talhabaris97@gmail.com>
2023-02-27 11:33:51 +0300

    Add project files.

---
+++ NLayer.Core/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Caching
+{
+    public abstract class BaseEntity //Soyut sınıflar newlenemez.Genelde abstract classlar projelerde ortak olan property veya methodlarımızı tanımladığımız yerlerdir.
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class 
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        // IQueryable yazmış olduğumuz sorgular direk veritabanına gitmez tolist vb. kullandığımızda veritabanına gider.Daha performanslı çalışır.
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        IQueryable<T> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+
+
+
+
+
+    }
+    
+
+     
+    
+}
