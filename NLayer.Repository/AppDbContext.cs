8ea3b8e322c01becdc110e1d31f30b7ba8135b1f
Talha Barış <talhabaris97@gmail.com>
2023-02-28 11:25:02 +0300

    Entity Configurations

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using NLayer.Repository.Configurations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NLayer.Repository
 {
-    internal class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration()); //Bu şekilde tek bitane alırız.
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
     }
 }
---
+++ NLayer.Repository/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configurations
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn(); //id değeri birer birer artar.
+            builder.Property(x=>x.Name).IsRequired().HasMaxLength(50);
+
+            builder.ToTable("Categories");
+            
+        }
+    }
+}
