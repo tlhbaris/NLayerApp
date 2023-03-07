317a2cb1c52d1aa41ad2793b17dc067d9cf48e7b
Talha Barış <talhabaris97@gmail.com>
2023-03-07 13:42:24 +0300

    ProductsWithCategory Endpoint

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -21,6 +21,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
+
 builder.Services.AddAutoMapper(typeof(MapProfile));  //AutoMapper eklendi.
 
 
