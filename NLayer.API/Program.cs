8b001d1a4b2f4e3ec815c5048d0299b53ad257bc
Talha Barış <talhabaris97@gmail.com>
2023-03-13 14:24:17 +0300

    AutoFac

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,8 +1,11 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NLayer.API.Filters;
 using NLayer.API.Middlewares;
+using NLayer.API.Modules;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
@@ -34,17 +37,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped(typeof(NotFoundFilter<>));
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-
-
-
 builder.Services.AddAutoMapper(typeof(MapProfile));  //AutoMapper eklendi.
 
 
@@ -57,6 +49,14 @@ builder.Services.AddDbContext<AppDbContext>(x =>
         //option.MigrationsAssembly("NLayer.Repository"); // Bu þekilde tip güvensiz oluyor üst satýrda dinamik olarak aldým.
     });
 });
+
+//AutoFac
+builder.Host.UseServiceProviderFactory
+    (new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModule()));
+
+
+
 
 var app = builder.Build();
 
